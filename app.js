2f9dd22f0468ae2cf3062be03675718af7aaccd2
Mason Zhang <mxz@gmail.com>
2018-05-09 00:41:10 +1000

    Remove jade using html directly

--- app.js
+++ app.js
@@ -8,15 +8,12 @@ var fs = require('fs');
 app.use(express.static(__dirname + '/public'));
 app.use('/libs', express.static(__dirname + '/node_modules'));
 app.use('/components', express.static(__dirname + '/bower_components'));
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
 
 server.listen(port=5000, host='192.168.1.142');
 console.log('Server running on: localhost:5000');
 
 app.get('/', function (req, res) {
-    //res.sendFile(__dirname + '/public/index.html');
-    res.render('home-server.jade');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', function (client) {
