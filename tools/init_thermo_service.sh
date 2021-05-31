07a8702e57a16c61b1bb23d4de8a2aa5240873bf
Mason Zhang <mason.xin.zhang@gmail.com>
2021-05-31 14:36:32 +0000

    setup pin15 direction out

--- tools/init_thermo_service.sh
+++ tools/init_thermo_service.sh
@@ -10,7 +10,7 @@ IS_RASPBERRY_PI=$(uname -a | grep -c 'raspberry')
 if [ $IS_RASPBERRY_PI -eq 1 ]; then
     #this is called from /etc/rc.d/rc.local
     echo "15" > /sys/class/gpio/export
-    echo "out" > /sys/class/gpio/gpio22/direction
+    echo "out" > /sys/class/gpio/gpio15/direction
     python $PROJECT_ROOT/python-scripts/thermo_monitor.py&
     python $PROJECT_ROOT/python-scripts/temperature_monitor.py&
     # node $PROJECT_ROOT/app.js >/dev/null&
